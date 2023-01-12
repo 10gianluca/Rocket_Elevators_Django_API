96400bab2044ca1c2b5dd520c1c08aef779f6429
10gianluca <113924900+10gianluca@users.noreply.github.com>
2023-01-11 22:58:19 -0500

    working

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
-    path('add/', views.addEmployee)
+    path('employee/<int:employee_id>/photo', views.add_employee_photo),
+    path('recognize', views.recognize_employee),
+    path('list', views.getData),
+    path('employee', views.add_employee)
 ]
--- api/views.py
+++ api/views.py
@@ -1,17 +1,51 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from api.serializers import EmployeesSerializer
 from base.models import Employees
-from .serializers import EmployeesSerializer
-
+import face_recognition
+@api_view(['POST'])
+def add_employee_photo(request, employee_id):
+    try:
+        employee = Employees.objects.get(id=employee_id)
+    except Employees.DoesNotExist:
+        return Response({"error": "Employee not found."}, status=404)
+    image_file = request.FILES.get('image')
+    if not image_file:
+        return Response({"error": "No image provided."}, status=400)
+    image = face_recognition.load_image_file(image_file)
+    face_encoding = face_recognition.face_encodings(image)[0]
+    employee.facial_keypoints = face_encoding.tolist()
+    employee.save()
+    return Response({"message": "Facial keypoints added successfully."}, status=201)
+@api_view(['POST'])
+def recognize_employee(request):
+    image_file = request.FILES.get('image')
+    if not image_file:
+        return Response({"error": "No image provided."}, status=400)
+    image = face_recognition.load_image_file(image_file)
+    face_encoding = face_recognition.face_encodings(image)[0]
+    matches = Employees.objects.filter(facial_keypoints__contains=face_encoding.tolist())
+    if matches.exists():
+        employee = matches.first()
+        serializer = EmployeesSerializer(employee)
+        return Response(serializer.data)
+    else:
+        return Response({"message": "Employee not found."}, status=404)
 @api_view(['GET'])
 def getData(request):
     employees = Employees.objects.all()
     serializer = EmployeesSerializer(employees, many=True)
     return Response(serializer.data)
-
 @api_view(['POST'])
-def addEmployee(request):
+def add_employee(request):
     serializer = EmployeesSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-    return  Response(serializer.data)
+        employee = serializer.save()
+        image_file = request.FILES.get('image')
+        if image_file:
+            image = face_recognition.load_image_file(image_file)
+            face_encoding = face_recognition.face_encodings(image)[0]
+            employee.facial_keypoints = face_encoding.tolist()
+            employee.save()
+        return  Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
